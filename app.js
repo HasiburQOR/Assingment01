2d0048d1f8a36ed8279f29cefecaf5f964362dff
HasiburQOR <hasibur.qor@gmail.com>
2023-03-23 02:04:47 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function multiplicationTable(num){
+    for (i = 1; i <= 10; i++)
+    
+    console.log("num x iteration = " + i * num)
+    
+    
+}
+
+multiplicationTable(parseInt(prompt("Enter an integer number")))
